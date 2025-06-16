cc29abb3471e4576664ec123da9d17bc63b0ca00
SahilCreate_ <144647329+Sahilcreate@users.noreply.github.com>
2025-06-16 18:09:58 +0530

    feat: checkout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
+import { useNavigate, useLocation } from "react-router-dom";
+
 const Header = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const isCheckout = location.pathname === "/checkout";
+    const isDisabled =isCheckout;
+
     return (
         <div>
             HEADER.
+            <button id="checkout-btn" disabled={isDisabled} onClick={() => navigate('checkout')}>checkout</button>
         </div>
     )
 }
